c78a9202cdc4b8de1be42df543f0e8d3eb381a44
JamesF <34280247+JamesFoltz@users.noreply.github.com>
2024-11-19 13:24:48 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const maxInput = document.getElementById("max-input");
+let max = 2;
+maxInput.value = urlParams.get("max") || max;
+max = Math.max(maxInput.value, 2);
+
+generateNumber();
+
+function generateNumber() {
+    max = Math.max(maxInput.value, 2);
+    urlParams.set("max", max);
+    var rand = Math.round(Math.random() * Math.max(max, 2));
+    document.getElementById("out").innerText = rand;
+    setClipboard(rand);
+}
+
+async function setClipboard(text) {
+    const type = "text/plain";
+    const blob = new Blob([text], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+    await navigator.clipboard.write(data);
+}
